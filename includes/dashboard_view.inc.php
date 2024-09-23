f404c559856cb7107a4b2abfa4de0ce0d1aacdb5
Luke Swiderski <Luke.Swiderski@gmail.com>
2024-09-23 09:40:36 -0400

    Update venue drop-down to include default select option instead of defaulting to back porch

--- includes/dashboard_view.inc.php
+++ includes/dashboard_view.inc.php
@@ -5,6 +5,7 @@
   function displayVenues(array $venues) { 
 ?>
 <select class="form-select w-auto" id="venue-select" name="venue-select">
+  <option value="">Select Venue</option>
   <?php foreach ($venues as $venue) { ?>
     <option value="<?php echo $venue['venue_name']; ?>"><?php echo $venue['venue_name']; ?></option>
   <?php } ?>
