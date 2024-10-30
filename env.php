b8d21d4fbf872570fad97cd2427f8f0b949b920d
Luke Swiderski <Luke.Swiderski@gmail.com>
2024-10-30 09:28:14 -0400

    Reset git tracking with proper gitignore

---
+++ env.php
@@ -0,0 +1,5 @@
+<?php
+
+$_ENV['EMAIL_USERNAME'] = 'luke.swiderski@gmail.com';
+$_ENV['EMAIL_PASSWORD'] = 'rgan cjus urhf kugl';
+$_ENV['TURNSTILE_SECRET_KEY'] = '0x4AAAAAAAdW9X5R9nYJ2oLUTM4pVwIYS3Y';
